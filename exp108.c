8e5c294545bcf00e594fbeb20e480592384188e5
Dhagash1206 <dhagashpandya2@gmail.com>
2024-11-28 14:09:42 +0530

    Update exp108.c

--- exp108.c
+++ exp108.c
@@ -13,8 +13,8 @@ void swap(char *a, char *b)
 }
 int main()
 {
-    char word1[100]= "Kavya";
-    char word2[] = "Bhatiya";
+    char word1[100]= "ram";
+    char word2[] = "sharma";
 
     char copy[10],concate[100];
     
